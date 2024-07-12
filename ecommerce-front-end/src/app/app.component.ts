10167de7de3ed21134682b8224213b2fe05a6023
egalvez <eduarddg@gmail.com>
2024-07-12 15:47:24 -0400

    main UI using flowbite

--- ecommerce-front-end/src/app/app.component.ts
+++ ecommerce-front-end/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import  HeaderComponent  from './shared/ui/header/header.component';
+import  SideComponent from './shared/ui/side/side.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SideComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'ecommerce-front-end';
